72e73b582abb3279590b28622540a16c7fae7aa1
Carlos Pinto <carlos.pinto@developen.cl>
2018-07-04 19:23:41 -0400

    Se corrige ruta de libs en gradle, y se genera clase aplicaion particular

---
+++ app/src/main/java/cl/usach/developen/demorfid/demorfid/DemoRFIDApplication.java
@@ -0,0 +1,16 @@
+package cl.usach.developen.demorfid.demorfid;
+
+import android.app.Application;
+
+import com.ugrokit.api.*;
+
+public class DemoRFIDApplication extends Application {
+    private Ugi ugi;
+    public Ugi getUgi() { return ugi; }
+
+    @Override public void onCreate() {
+        super.onCreate();
+        ugi = Ugi.createSingleton(this);
+        ugi.openConnection();
+    }
+}
